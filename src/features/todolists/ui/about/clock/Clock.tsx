826c067773d6c713fbc3b258f085fb1c737893f5
Kytyzovns <kytyzovns@gmail.com>
2024-10-08 23:18:33 +0300

    some refactoring

--- src/features/todolists/ui/about/clock/Clock.tsx
+++ src/features/todolists/ui/about/clock/Clock.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Button from "@mui/material/Button";
-import "./clock.css"
+import "./analogClock/clock.css"
+import {AnalogClock} from "./analogClock/AnalogClock";
 
 const getDoubleDigitTime = (time: number) => time < 10 ? '0' + time : time;
 
@@ -24,9 +25,6 @@ export const Clock = () => {
     const minutes = time.getMinutes();
     const hours = time.getHours();
 
-    const secondDegrees = (seconds / 60) * 360
-    const minuteDegrees = ((minutes + seconds/60) / 60) * 360
-    const hourDegrees = ((hours % 12 + (minutes/60)) / 12) * 360
 
     const timeFormatChangeHandler = () => {
         setTimeFormat(prevState => prevState === "12" ? "24" : "12");
@@ -41,22 +39,7 @@ export const Clock = () => {
             {timeFormat === "12" && <span>{time.getHours() >= 12 ? " PM" : " AM"}</span>}
             <button onClick={timeFormatChangeHandler}>format</button>
 
-            <div className="clock">
-                <div className="clock-face">
-                    <div
-                        className="hand hour-hand"
-                        style={{transform: `rotate(${hourDegrees + 90}deg)`}}
-                    />
-                    <div
-                        className="hand minute-hand"
-                        style={{transform: `rotate(${minuteDegrees + 90}deg)`}}
-                    />
-                    <div
-                        className="hand second-hand"
-                        style={{transform: `rotate(${secondDegrees + 90}deg)`}}
-                    />
-                </div>
-            </div>
+            <AnalogClock time={time} />
         </div>
     );
 };
